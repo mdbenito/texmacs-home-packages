d26d32d66460b0c7b43df3bd850e0a36ae36b242
Miguel de Benito Delgado <m.debenito.d@gmail.com>
2017-06-27 12:07:59 +0200

    Allow individually setting footer left/middle text. Unify border colors

--- beamer-footer.ts
+++ beamer-footer.ts
@@ -33,9 +33,6 @@
 
   <assign|title-color|white> --- Text color in the top title bar
 
-  <assign|tit-border-color|#307fcc> --- Color of the bottom border for the
-  top title bar
-
   <assign|tit-border-height|0.002pag> --- Height of the bottom border for the
   top title bar
 
@@ -43,36 +40,29 @@
 
   <assign|footer-color|white> --- Text color in the footer bar
 
-  <assign|ftr-border-color|#307fcc> --- Color of the bottom border for the
-  footer bar
-
   <assign|ftr-border-height|0.001pag> --- Height of the top border for the
   footer bar
+
+  <assign|bar-border-color|#307fcc> --- Color of the border for the title and
+  footer bars
 
   <assign|math-color|#0C2754bf>
 
   <assign|strong-color|#0C2754bf>
 
-  <assign|screens-running-footer|> --- Content of the middle section for all
-  footers in the presentation
-
   \;
 
   --- Add our parameters to the configurable style parameters
 
-  <drd-props|tit-border-color|macro-parameter|color>
-
   <drd-props|tit-border-height|macro-parameter|length>
+
+  <drd-props|ftr-border-height|macro-parameter|length>
 
   <drd-props|footer-color|macro-parameter|color>
 
   <drd-props|footer-bar-color|macro-parameter|color>
 
-  <drd-props|ftr-border-color|macro-parameter|color>
-
-  <drd-props|ftr-border-height|macro-parameter|length>
-
-  <drd-props|screens-running-footer|macro-parameter|string>
+  <drd-props|bar-border-color|macro-parameter|color>
 
   <\active*>
     <\src-comment>
@@ -84,7 +74,7 @@
   </active*>
 
   <assign|cover-title|<\macro|body>
-    <\with|ornament-shape|classic|ornament-color|<value|title-bar-color>|ornament-sunny-color|<value|tit-border-color>|ornament-vpadding|1.7bls|ornament-shadow-color|<value|tit-border-color>>
+    <\with|ornament-shape|classic|ornament-color|<value|title-bar-color>|ornament-sunny-color|<value|bar-border-color>|ornament-vpadding|1.7bls|ornament-shadow-color|<value|bar-border-color>>
       <\ornamented>
         <with|color|<value|title-color>|<htab|5mm><really-huge|<arg|body>><htab|5mm>>
       </ornamented>
@@ -92,7 +82,7 @@
   </macro>>
 
   <assign|cover-footer|<\macro|left|right>
-    <\with|ornament-shape|classic|ornament-color|<value|footer-bar-color>|ornament-sunny-color|<value|ftr-border-color>|ornament-shadow-color|<value|ftr-border-color>>
+    <\with|ornament-shape|classic|ornament-color|<value|footer-bar-color>|ornament-sunny-color|<value|bar-border-color>|ornament-shadow-color|<value|bar-border-color>>
       <\ornamented>
         <em|<with|color|<value|footer-color>|<very-small|<arg|left>><htab|5mm><very-small|<arg|right>>>>
       </ornamented>
@@ -101,12 +91,12 @@
 
   <assign|cover|<\macro|title|author|sub-left|sub-right>
     <\with|par-par-sep|0fn|par-sep|0fn|par-ver-sep|0fn|par-line-sep|0fn>
-      <with|color|<value|tit-border-color>|<resize|<tabular*|<tformat|<twith|table-width|1par>|<twith|table-hmode|exact>|<cwith|1|1|1|-1|cell-background|<value|title-bar-color>>|<cwith|1|1|1|1|cell-halign|c>|<cwith|1|1|1|1|cell-hyphen|t>|<twith|table-valign|T>|<twith|table-vmode|auto>|<cwith|1|1|1|1|cell-bborder|5px>|<cwith|1|1|1|1|cell-height|0.2pag>|<cwith|1|1|1|1|cell-vmode|exact>|<cwith|1|1|1|1|cell-valign|c>|<table|<row|<\cell>
+      <with|color|<value|bar-border-color>|<resize|<tabular*|<tformat|<twith|table-width|1par>|<twith|table-hmode|exact>|<cwith|1|1|1|-1|cell-background|<value|title-bar-color>>|<twith|table-valign|T>|<twith|table-vmode|auto>|<cwith|1|1|1|1|cell-bborder|5px>|<cwith|1|1|1|1|cell-vmode|exact>|<cwith|1|1|1|1|cell-valign|c>|<cwith|1|1|1|1|cell-halign|c>|<cwith|1|1|1|1|cell-lsep|0.4em>|<cwith|1|1|1|1|cell-rsep|0.4em>|<cwith|1|1|1|1|cell-bsep|0.4em>|<cwith|1|1|1|1|cell-tsep|0.4em>|<cwith|1|1|1|1|cell-hyphen|t>|<table|<row|<\cell>
         <with|color|<value|title-color>|math-color|<value|title-color>|font-series|bold|font-size|1.5|<htab|5mm><arg|title><htab|5mm>>
       </cell>>>>>||0em||>>
 
-      <shift|<with|color|<value|tit-border-color>|<tabular*|<tformat|<twith|table-width|1par>|<twith|table-hmode|exact>|<cwith|1|1|1|-1|cell-background|<value|footer-bar-color>>|<cwith|1|1|1|1|cell-halign|c>|<cwith|1|1|1|1|cell-hyphen|t>|<twith|table-valign|b>|<twith|table-height|0.02pag>|<twith|table-vmode|exact>|<cwith|1|1|1|1|cell-tsep|0em>|<cwith|1|1|1|1|cell-bsep|0em>|<cwith|1|1|1|1|cell-tborder|5px>|<table|<row|<\cell>
-        <with|color|<value|title-color>|math-color|<value|title-color>|font-size|0.59|<arg|sub-left><htab|5mm><arg|sub-right>>
+      <shift|<with|color|<value|bar-border-color>|<tabular*|<tformat|<twith|table-width|1par>|<twith|table-hmode|exact>|<cwith|1|1|1|-1|cell-background|<value|footer-bar-color>>|<cwith|1|1|1|1|cell-halign|c>|<cwith|1|1|1|1|cell-hyphen|t>|<twith|table-valign|b>|<twith|table-height|0.02pag>|<twith|table-vmode|exact>|<cwith|1|1|1|1|cell-tsep|0em>|<cwith|1|1|1|1|cell-bsep|0em>|<cwith|1|1|1|1|cell-tborder|5px>|<table|<row|<\cell>
+        <with|color|<value|footer-color>|math-color|<value|footer-color>|font-size|0.59|<arg|sub-left><htab|5mm><arg|sub-right>>
       </cell>>>>>>|0mm|<minus|2h|1pag>>
 
       <shift|<small|<strong|<arg|author>>>|<minus|0.5par|0.5w>|<minus|0.5pag>>
@@ -133,22 +123,32 @@
   <assign|screens-summary|<macro|body|<if|<greater|<screens-arity|<quote-arg|body>>|0>|<plus|<screens-index|<quote-arg|body>>|1>
   of <screens-arity|<quote-arg|body>>>>>
 
-  <assign|ftr|<macro|left|center|right|<with|color|<value|ftr-border-color>|<tabular*|<tformat|<twith|table-width|1par>|<twith|table-hmode|exact>|<cwith|1|1|1|-1|cell-background|<value|footer-bar-color>>|<cwith|1|1|1|1|cell-halign|c>|<cwith|1|1|1|1|cell-tsep|0ex>|<style-with|src-compact|all|<cwith|1|1|1|1|cell-bsep|0em>>|<cwith|1|1|1|1|cell-tborder|<value|ftr-border-height>>|<cwith|1|1|1|1|cell-vmode|auto>|<twith|table-height|0.03pag>|<twith|table-vmode|exact>|<cwith|1|1|1|1|cell-hyphen|t>|<twith|table-lsep|0>|<twith|table-rsep|0>|<twith|table-bsep|0>|<twith|table-tsep|0>|<twith|table-bborder|0>|<cwith|1|1|1|1|cell-valign|c>|<twith|table-valign|f>|<table|<row|<\cell>
+  --- Helper macro
+
+  <assign|ftr|<macro|left|center|right|<with|color|<value|bar-border-color>|<tabular*|<tformat|<twith|table-width|1par>|<twith|table-hmode|exact>|<cwith|1|1|1|-1|cell-background|<value|footer-bar-color>>|<cwith|1|1|1|1|cell-halign|c>|<cwith|1|1|1|1|cell-tsep|0ex>|<style-with|src-compact|all|<cwith|1|1|1|1|cell-bsep|0em>>|<cwith|1|1|1|1|cell-tborder|<value|ftr-border-height>>|<cwith|1|1|1|1|cell-vmode|auto>|<twith|table-height|0.03pag>|<twith|table-vmode|exact>|<cwith|1|1|1|1|cell-hyphen|t>|<twith|table-lsep|0>|<twith|table-rsep|0>|<twith|table-bsep|0>|<twith|table-tsep|0>|<twith|table-bborder|0>|<cwith|1|1|1|1|cell-valign|c>|<twith|table-valign|f>|<table|<row|<\cell>
     <with|color|<value|footer-color>|math-color|<value|footer-color>|font-size|0.59|<arg|left><htab|5mm><arg|center><htab|5mm><with|font-size|0.39|<arg|right>>>
   </cell>>>>>>>>
 
+  \;
+
+  --- Helper macro
+
+  <assign|hdr|<macro|left|center|right|<with|color|<value|bar-border-color>|<resize|<tabular*|<tformat|<twith|table-width|1par>|<twith|table-hmode|exact>|<cwith|1|1|1|-1|cell-background|<value|title-bar-color>>|<cwith|1|1|1|1|cell-halign|c>|<cwith|1|1|1|1|cell-hyphen|t>|<twith|table-valign|T>|<twith|table-vmode|auto>|<cwith|1|1|1|1|cell-height|0.05pag>|<cwith|1|1|1|1|cell-vmode|exact>|<cwith|1|1|1|1|cell-bborder|<value|tit-border-height>>|<table|<row|<\cell>
+    <with|color|<value|title-color>|math-color|<value|title-color>|font-series|bold|<arg|left><htab|5mm><arg|center><htab|5mm><arg|right>>
+  </cell>>>>>||0em||>>>>
+
+  \;
+
   --- The macro constructing the top and bottom bars. Note that the top right
-  content (the page counter) is disabled with a phantom macro. Instead it is
+  content (the page counter) is disabled within a phantom. Instead it is
   placed in the footer
 
-  <assign|tit|<\macro|head|foot>
+  <assign|tit|<\macro|head|foot-left|foot-center>
     <\with|par-left|<minus|<value|page-screen-left>>|par-right|<minus|<value|page-screen-right>>|par-par-sep|0fn|par-sep|0fn|par-ver-sep|0fn|par-line-sep|0fn>
       <\reduce-by>
-        <shift|<with|color|<value|tit-border-color>|<resize|<tabular*|<tformat|<twith|table-width|1par>|<twith|table-hmode|exact>|<cwith|1|1|1|-1|cell-background|<value|title-bar-color>>|<cwith|1|1|1|1|cell-halign|c>|<cwith|1|1|1|1|cell-hyphen|t>|<twith|table-valign|T>|<twith|table-vmode|auto>|<cwith|1|1|1|1|cell-height|0.05pag>|<cwith|1|1|1|1|cell-vmode|exact>|<cwith|1|1|1|1|cell-bborder|<value|tit-border-height>>|<table|<row|<\cell>
-          <with|color|<value|title-color>|math-color|<value|title-color>|font-series|bold|<title-left|<arg|head>><htab|5mm><arg|head><htab|5mm><phantom|<title-right|xxx>>>
-        </cell>>>>>||0em||>>|0mm|<value|page-screen-top>>
+        <shift|<hdr|<title-left|<arg|head>>|<arg|head>|<phantom|<title-right|xxx>>>|0mm|<value|page-screen-top>>
 
-        <shift|<ftr|<value|screens-running-footer>|<arg|foot>|<title-right|xxx>>|0mm|<minus|0.959pag>>
+        <shift|<ftr|<arg|foot-left>|<arg|foot-center>|<title-right|xxx>>|0mm|<minus|0.959pag>>
       <|reduce-by>
         1ex
       </reduce-by>
@@ -158,9 +158,9 @@
   --- FIXME: this macro is hard to navigate with the cursor. Also, it leaves
   lots of empty space at the top of the document.
 
-  <assign|btit|<\macro|head>
+  <assign|btit|<\macro|left|center>
     <\with|par-left|<minus|<value|page-screen-left>>|par-right|<minus|<value|page-screen-right>>|par-par-sep|0fn|par-sep|0fn|par-ver-sep|0fn|par-line-sep|0fn|par-hor-sep|0fn|par-mode|left>
-      <reduce-by|<shift|<ftr|<value|screens-running-footer>|<arg|head>|<title-right|xxx>>|0mm|<minus|0.994pag>>|1ex>
+      <reduce-by|<shift|<ftr|<arg|left>|<arg|center>|<title-right|xxx>>|0mm|<minus|0.994pag>>|1ex>
     </with>
   </macro>>
 
